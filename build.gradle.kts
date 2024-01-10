e6278e13158b60df9ab6e478e3ea8244c39e5f96
Jinny <33302351+jinny-hm@users.noreply.github.com>
2024-01-10 19:18:22 +0900

    jooq 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,10 @@
-import org.jooq.codegen.GenerationTool
-import org.jooq.meta.jaxb.Configuration
-import org.jooq.meta.jaxb.Database
-import org.jooq.meta.jaxb.Generate
-import org.jooq.meta.jaxb.Generator
-import org.jooq.meta.jaxb.Jdbc
-import org.jooq.meta.jaxb.Target
+import org.jooq.meta.jaxb.Logging
 
 plugins {
     java
     id("org.springframework.boot") version "3.2.1"
     id("io.spring.dependency-management") version "1.1.4"
+    id("org.jooq.jooq-codegen-gradle") version "3.19.1"
 }
 
 group = "gim"
@@ -20,90 +15,48 @@ java {
 }
 
 repositories {
-    mavenLocal()
     mavenCentral()
 }
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-jooq:3.2.1")
 
+    runtimeOnly("org.postgresql:postgresql:42.2.16")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.1")
 
-    compileOnly("org.jooq:jooq:3.18.8")
-
-    runtimeOnly("org.postgresql:postgresql:42.2.16")
+    jooqCodegen("org.postgresql:postgresql:42.2.16")
 }
 
+jooq {
+    configuration {
+        logging = Logging.WARN
 
-buildscript {
-    repositories {
-        mavenLocal()
-        mavenCentral()
-    }
+        jdbc {
+            driver = "org.postgresql.Driver"
+            url = "jdbc:postgresql://localhost:5432/jinny"
+            user = "jinny"
+            password = ""
+        }
 
-    dependencies {
-        classpath("org.jooq:jooq-codegen:3.18.8")
-        classpath("org.postgresql:postgresql:42.2.16")
+        generator {
+            database {
+                name = "org.jooq.meta.postgres.PostgresDatabase"
+                includes = ".*"
+                excludes = ""
+                inputSchema = "public"
+            }
+            generate {
+                isPojos = true
+                isDaos = true
+            }
+            target {
+                packageName = "gim.postgresql.jooq.model"
+                directory = "src/main/java"
+            }
+        }
     }
 }
-
-
-GenerationTool.generate(
-    Configuration().withJdbc(
-        Jdbc()
-            .withDriver("org.postgresql.Driver")
-            .withUrl("jdbc:postgresql://localhost:5432/jinny")
-            .withUser("jinny")
-            .withPassword("")
-    ).withGenerator(
-        Generator()
-            .withDatabase(Database())
-            .withGenerate(
-                Generate()
-                    .withPojos(true)
-                    .withDaos(true)
-            )
-            .withTarget(
-                Target()
-                    .withPackageName("gim.postgresql.jooq.model")
-                    .withDirectory("src/main/java")
-            )
-    )
-)
-
-
-//jooq {
-//    configuration {
-//        logging = Logging.WARN
-//
-//        jdbc {
-//            driver = "org.postgresql.Driver"
-//            url = "jdbc:postgresql://localhost:5432/jinny"
-//            user = "jinny"
-//            password = ""
-//        }
-//
-//        generator {
-//            name = "org.jooq.codegen.JavaGenerator"
-//
-//            database {
-//                name = "org.jooq.meta.postgres.PostgresDatabase"
-//                includes = ".*"
-//                excludes = ""
-//                inputSchema = "public"
-//            }
-//
-//            generator { }
-//            target {
-//                packageName = "gim.postgresql.jooq.model"
-//                directory = "src/main/java"
-//            }
-//
-//            generate {
-//            }
-//        }
-//    }
-//}
 
 tasks.withType<Test> {
     useJUnitPlatform()
